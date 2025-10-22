f77a1c16069b570a05ae4e33710f6c33c4d8e9ba
Atharv-git2004 <atharvpaleri@gmail.com>
2025-10-22 09:02:31 +0530

    index

---
+++ index.js
@@ -0,0 +1,21 @@
+//import json-server library
+const jsonServer =  require('json-server')
+
+// create a express like server instance
+const criketServer=jsonServer.create()
+
+// tell json server to use dataBase.json as its data source
+const router=jsonServer.router('dataBase.json')
+
+//add useful middleware -cors,  logging etc
+const middleware = jsonServer.defaults()
+criketServer.use(middleware)
+criketServer.use(router)
+
+
+const PORT=process.env.PORT || 3000
+
+criketServer.listen(PORT,()=>{
+    console.log(`rbserver running at port ${PORT} and waiting for client request`);
+    
+})
